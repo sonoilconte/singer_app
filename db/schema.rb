bb803a9cdd75ac1e2eea6d372d0548ad3e5ab8df
sonoilconte <shammersinger@yahoo.com>
2018-06-24 15:37:41 -0700

    Add users model and bcrypt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_11_012036) do
+ActiveRecord::Schema.define(version: 2018_06_24_221340) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2018_06_11_012036) do
     t.string "bio"
     t.string "email"
     t.string "management"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
