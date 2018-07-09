1ef7482e6ad69ab455f8bc52968bcfd03be5209b
sonoilconte <shammersinger@yahoo.com>
2018-07-08 17:43:04 -0700

    Use partials for menu and welcome user

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,9 @@
 class ArtistsController < ApplicationController
   def index
+    if session[:user_id]
+      @currentUser = User.find(session[:user_id])
+      p "current user is #{@currentUser}"
+    end
     @artists = Artist.all
   end
 end
