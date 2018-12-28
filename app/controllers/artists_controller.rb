b0b22514bd48ad027c3b659c2803f3dde7279ae0
sonoilconte <shammersinger@yahoo.com>
2018-12-27 16:11:27 -0800

    Show artist page

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,4 +3,10 @@ class ArtistsController < ApplicationController
     current_user
     @artists = Artist.all
   end
+
+  def show
+    artist_id = params[:id]
+    @artist = Artist.find_by(id: artist_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'welcome#index'
   get '/artists' => 'artists#index', as: 'artists'
+  get '/artists/:id' => 'artists#show'
 
   # Sessions
   get '/login' => 'sessions#new'
