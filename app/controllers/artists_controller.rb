eb2fff1ac87fba630645108a574f79d8142cf5db
sonoilconte <shammersinger@yahoo.com>
2018-12-28 12:04:06 -0800

    Basic structure for world-facing pages

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,25 +1,55 @@
 class ArtistsController < ApplicationController
+
   def index
     current_user
     @artists = Artist.all
   end
 
   def show
+    current_user
     artist_id = params[:id]
     @artist = Artist.find_by(id: artist_id)
   end
 
   def show_default
-    @artist = Artist.find_by(id: 2)
+    current_user
+    default_artist
+  end
+
+  def show_default_bio
+    current_user
+    default_artist
+  end
+
+  def show_default_resume
+    current_user
+    default_artist
+  end
+
+  def show_default_media
+    current_user
+    default_artist
+  end
+
+  def show_default_contact
+    current_user
+    default_artist
   end
 
   # will make this DRYer
   def schedule
+    current_user
     artist_id = params[:id]
     @artist = Artist.find_by(id: artist_id)
     # This line simply demonstrating we can access any model
     # Will access the events for a given artist here
     p User.all
+  end
+
+  private
+
+  def default_artist
+    @artist = Artist.find_by(id: 2)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # get '/' => 'welcome#index'
   get '/' => 'artists#show_default'
+  get '/bio' => 'artists#show_default_bio'
+  get '/resume' => 'artists#show_default_resume'
+  get '/listen' => 'artists#show_default_media'
+  get '/contact' => 'artists#show_default_contact'
+  # get '/schedule' => 'artists#show_default_schedule'
   get '/artists' => 'artists#index', as: 'artists'
   get '/artists/:id' => 'artists#show'
   get 'artists/:id/schedule' => 'artists#schedule'
+
+  # Welcome
+  get '/welcome' => 'welcome#index'
+
 
   # Sessions
   get '/login' => 'sessions#new'
