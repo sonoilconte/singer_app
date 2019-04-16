61f509c3cd0848f5b001f3efd2913950608405f7
sonoilconte <shammersinger@yahoo.com>
2019-04-16 11:18:32 -0700

    Add basic images gallery view Clean up .each blocks for images Clean up single vs. double quotes throughout

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   # Routes for the default artist/user for the instance of the application
+  # visible to the end user
   get '/' => 'artists#show_default'
   get '/bio' => 'artists#show_default_bio'
+  get '/schedule' => 'artists#show_default_schedule'
   get '/resume' => 'artists#show_default_resume'
   get '/listen' => 'artists#show_default_media'
+  get '/gallery' => 'artists#show_default_images'
   get '/contact' => 'artists#show_default_contact'
-  # get '/schedule' => 'artists#show_default_schedule'
+
+  # Routes for administrator managing content
   get '/artists' => 'artists#index', as: 'artists'
   get '/artists/new' => 'artists#new'
   get '/artists/:id' => 'artists#show'
@@ -20,7 +25,6 @@ Rails.application.routes.draw do
 
   # Welcome
   get '/welcome' => 'welcome#index'
-
 
   # Sessions
   get '/login' => 'sessions#new'
