d577f7a658f5190c3e2625da5f8f0ada6b30eefc
sonoilconte <shammersinger@yahoo.com>
2019-04-17 16:00:46 -0700

    Add local.env.yml and ability to load ENV variables Access ENV variables for primary user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::Base
   def current_user
     if session[:user_id]
       @current_user = User.find(session[:user_id])
-      # p "current user is #{@current_user.email}"
+      p "current user is #{@current_user.email}"
+      @current_user
     else
       nil
     end
