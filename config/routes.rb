e225a419f3ebe4514dd86bf5981522fbecaaa3b2
sonoilconte <shammersinger@yahoo.com>
2019-04-23 21:52:13 -0700

    Add edit and update routes, controllers, views, clean up links

--- config/routes.rb
+++ config/routes.rb
@@ -17,18 +17,20 @@ Rails.application.routes.draw do
   # Routes for administrator managing content
   get '/artists' => 'artists#index', as: 'artists'
   get '/artists/new' => 'artists#new'
-  get '/artists/:id' => 'artists#show'
   post '/artists/new' => 'artists#create'
+  get '/artists/:id' => 'artists#show'
   get '/artists/:id/edit' => 'artists#edit', as: 'artists_edit'
   put '/artists/:id' => 'artists#update'
   delete '/artists/:id' => 'artists#delete', as: 'artists_delete'
   delete 'artists/:id/attachments/:image_id' => 'artists#delete_image'
 
   # Routes for administration of images associated with artist
+  get '/artists/:id/images' => 'images#index'
   get '/artists/:id/images/new' => 'images#new'
   post '/artists/:id/images/new' => 'images#create'
   get '/artists/:id/images/:image_id' => 'images#show'
-  get '/artists/:id/images' => 'images#index'
+  get '/artists/:id/images/:image_id/edit' => 'images#edit'
+  put '/artists/:id/images/:image_id' => 'images#update'
   delete '/artists/:id/images/:image_id' => 'images#delete'
 
   # Welcome
