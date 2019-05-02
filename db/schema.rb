794073359b55de84d134f8a8ce590a0737f4bdde
sonoilconte <shammersinger@yahoo.com>
2019-05-02 16:10:18 -0700

    Add media column to Artists, render html for iframe embeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_25_214828) do
+ActiveRecord::Schema.define(version: 2019_05_02_182328) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2019_04_25_214828) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "schedule"
+    t.text "media"
   end
 
   create_table "artists_users", id: false, force: :cascade do |t|
