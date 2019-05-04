ee601d5b6a1b1cc40356f593f30768da0cbe300b
sonoilconte <shammersinger@yahoo.com>
2019-05-04 15:50:58 -0700

    Integrate Home, Biography views with styles Add helper nav_link for applying active class to current page link in menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def nav_link(display_text, path)
+    class_name = current_page?(path) ? 'active' : nil
+    # Create li tag with class active if the link is to the current page
+    content_tag(:li, :class => class_name) do
+      link_to display_text, path
+    end
+  end
+
+  def tester(arg)
+    p "Here is the arg #{arg}"
+  end
+
 end
