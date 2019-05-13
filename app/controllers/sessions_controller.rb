e4fdaf358441f4f20857e49a5f022da33b42d6df
sonoilconte <shammersinger@yahoo.com>
2019-05-13 15:18:58 -0700

    Switch database from sqlite3 to postgres Add seeds.rb file for seeding admin, primary users and primary artist

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:user][:password])
       session[:user_id] = user.id
       p 'access permitted, session[:user_id]', session[:user_id]
-      redirect_to "/artists/#{ENV['PRIMARY_USER_ID']}/edit"
+      redirect_to "/artists/#{ENV['PRIMARY_ARTIST_ID']}/edit"
     else
       p 'access denied'
       redirect_to '/login'
