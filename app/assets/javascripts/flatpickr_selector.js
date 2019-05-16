253ddc2b229b534aa262ca890840a0627c54b24c
sonoilconte <shammersinger@yahoo.com>
2019-05-16 15:30:38 -0700

    Adjust pdf view for mobile

--- app/assets/javascripts/flatpickr_selector.js
+++ app/assets/javascripts/flatpickr_selector.js
@@ -1,4 +1,5 @@
 document.addEventListener('turbolinks:load', function() {
+  console.log("turbolinks load!");
   var el = document.getElementById('flatpickr');
   var datetime = el.getAttribute('datetime');
   flatpickr(el, {
@@ -7,4 +8,9 @@ document.addEventListener('turbolinks:load', function() {
     altFormat: 'F j, Y h:i K',
     defaultDate: datetime
   });
+  if (el.nextElementSibling && el.nextElementSibling.nextElementSibling) {
+    // flatpickr has injected extra input due to turbolinks, remove extra input sibling
+    var extraSibling = el.nextElementSibling.nextElementSibling;
+    extraSibling.parentNode.removeChild(extraSibling);
+  }
 });
