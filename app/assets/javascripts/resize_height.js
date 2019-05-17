125070d3eb3cf3bc067e392a6e368486f3ed4e02
sonoilconte <shammersinger@yahoo.com>
2019-05-16 18:19:32 -0700

    Add Javascript for sizing div according to exact lines of text

---
+++ app/assets/javascripts/resize_height.js
@@ -0,0 +1,7 @@
+// var divHeight = $('.style_right_side_content').height();
+// $('.style_left_side_content').css('min-height', divHeight+'px');
+
+// $('turbolinks:load').ready(function() {
+//   var divHeight = $('.featured-image-frame').height();
+//   $('.home-bio').css('height', divHeight + 'px');
+// });
