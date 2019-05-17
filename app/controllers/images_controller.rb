65cfafbee7cf7ac5973c9e0e2f12c1e0fc4fab62
sonoilconte <shammersinger@yahoo.com>
2019-05-17 11:38:24 -0700

    Clean up redirects, logo link

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -19,7 +19,7 @@ class ImagesController < ApplicationController
   def create
     redirect_unauthorized_user(current_user, params[:id])
     image = Image.create!(image_params)
-    redirect_to "/artists/#{params[:id]}/images/#{image.id}"
+    redirect_to "/artists/#{params[:id]}/images"
   end
 
   def edit
@@ -41,7 +41,7 @@ class ImagesController < ApplicationController
   def delete
     redirect_unauthorized_user(current_user, params[:id])
     Image.delete(params[:image_id])
-    redirect_to "/artists/#{params[:id]}/images/"
+    redirect_to "/artists/#{params[:id]}/images"
   end
 
   private
